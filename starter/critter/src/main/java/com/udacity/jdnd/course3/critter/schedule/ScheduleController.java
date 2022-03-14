7960f2df3d4d74ca8b6f475f98feafbaa7bb3180
Hornung Frank <frank.hornung@de.bosch.com>
2022-03-14 11:37:25 +0100

    testSchedulePetsForServiceWithEmployee-working

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,8 +1,19 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.pet.PetService;
+import com.udacity.jdnd.course3.critter.user.CustomerService;
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeService;
+import com.udacity.jdnd.course3.critter.user.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
+
 
 /**
  * Handles web requests related to Schedules.
@@ -11,28 +22,91 @@ import java.util.List;
 @RequestMapping("/schedule")
 public class ScheduleController {
 
+    private final EmployeeService employeeService;
+    private final CustomerService customerService;
+    private final PetService petService;
+    private final ScheduleService scheduleService;
+
+    //@Autowired
+    public ScheduleController(EmployeeService employeeService, CustomerService customerService, PetService petService, ScheduleService scheduleService){
+        this.customerService=customerService;
+        this.employeeService=employeeService;
+        this.petService=petService;
+        this.scheduleService=scheduleService;
+    }
+
+    // create a schedule for some pets that get taken care by a team of employees with specific skills on a specific date.
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
-        throw new UnsupportedOperationException();
+        return schedulePojoToDTO(scheduleService.save(scheduleDTOToPojo(scheduleDTO)));
     }
 
+    // return all schedules that exist
     @GetMapping
     public List<ScheduleDTO> getAllSchedules() {
-        throw new UnsupportedOperationException();
+        List<ScheduleDTO> scheduleDTOs = new ArrayList<ScheduleDTO>();
+        for(Schedule s : scheduleService.getAllSchedules()){
+            scheduleDTOs.add(schedulePojoToDTO(s));
+        }
+        return scheduleDTOs;
     }
 
+    // a pet can have multiple schedules when it is taken care by an employee
+    // this method is to return the list of schedules for a specific pet.
     @GetMapping("/pet/{petId}")
     public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
         throw new UnsupportedOperationException();
     }
 
+    // an employee can have multiple schedules for taking care of multiple pets
+    // this method is to return the list of schedules for a specific employee
     @GetMapping("/employee/{employeeId}")
     public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
         throw new UnsupportedOperationException();
     }
 
+    // a customer may have booked several petting schedules for his pets
+    // return all the schedules booked by a specific customer
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
         throw new UnsupportedOperationException();
+    }
+
+    public Schedule scheduleDTOToPojo(ScheduleDTO dto) {
+        Schedule schedule = new Schedule();
+        BeanUtils.copyProperties(dto,schedule);
+
+        List<Employee> employeeList = new ArrayList<Employee>();
+        for (Long employeeId : dto.getEmployeeIds()){
+            employeeList.add(employeeService.getEmployee(employeeId));
+        }
+        schedule.setEmployees(employeeList);
+
+        List<Pet> petList = new ArrayList<Pet>();
+        for (Long petId : dto.getPetIds()){
+            petList.add(petService.getPetById(petId));
+        }
+        schedule.setPets(petList);
+
+        return schedule;
+    }
+
+    public ScheduleDTO schedulePojoToDTO(Schedule schedule){
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        BeanUtils.copyProperties(schedule, scheduleDTO);
+
+        List<Long> employeeIds = new ArrayList<Long>();
+        for(Employee e : schedule.getEmployees()){
+            employeeIds.add(e.getId());
+        }
+        scheduleDTO.setEmployeeIds(employeeIds);
+
+        List<Long> petIds = new ArrayList<Long>();
+        for(Pet p : schedule.getPets()){
+            petIds.add(p.getId());
+        }
+        scheduleDTO.setPetIds(petIds);
+
+        return scheduleDTO;
     }
 }
